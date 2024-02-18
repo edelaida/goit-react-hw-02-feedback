efa2eb8df0f3d55eee67160d3adfd5e02e73a2a5
Levina Tani <edelaida@gmail.com>
2024-02-18 23:39:45 +0200

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,48 @@
+import React from 'react';
 import { Statistics } from './Statistics/Statistics.jsx';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
+import { Button } from './Button.jsx';
 
-export const App = () => {
-  return (
-    <div>
-      <Statistics />
-    </div>
-  );
-};
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-// export default App;
+  onLeaveFeedback = value => {
+    this.setState({ [value]: this.state[value] + 1 });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return (this.state.good / this.countTotalFeedback()) * 100;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <Button></Button>
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.onLeaveFeedback}
+        ></FeedbackOptions>
+        {this.countTotalFeedback() === 0 ? (
+          <p>There is no feedback</p>
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        )}
+      </div>
+    );
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,20 @@
+import React from 'react';
 import s from './Statistics.module.css';
 
-export const Statistics = () => {
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
   return (
-    <section className={s.headSection}>
-      <h1>feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
-    </section>
+    <div className={s.stat}>
+      <p className={s.text}>Good: {good}</p>
+      <p className={s.text}>Neutral: {neutral}</p>
+      <p className={s.text}>Bad: {bad}</p>
+      <p className={s.textsum}>Total: {total}</p>
+      <p className={s.textsum}>Positive feedback: {positivePercentage}</p>
+    </div>
   );
 };
